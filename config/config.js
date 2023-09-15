ca97dc583dca66389ab8364709f19e5b1a1075cc
Anmol jagota <101393363+Anmoljagota@users.noreply.github.com>
2023-09-15 14:50:28 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // General configuration (e.g., environment variables) 
 PORT=8000
-database="social_Media_App_Backend"
+database="Dashboard_Backend"
 username= "root"
 password= "tanya123"
 host= "localhost" 
